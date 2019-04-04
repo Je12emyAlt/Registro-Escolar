a97dd9fac85303061f0a8f7be2d147e45b64e3b3
Jeremy <jeremyzelaya@hotmail.es>
2019-04-03 23:50:56 -0600

    Basic configuration for metodos_datos

---
+++ capa_datos/configuracion/clases/configuracion.vb
@@ -0,0 +1,8 @@
+﻿Public Class configuracion
+    Shared _cadenaConexion As String = ""
+    Public Shared ReadOnly Property CadenaConexion() As String
+        Get
+            Return _cadenaConexion
+        End Get
+    End Property
+End Class
---
+++ capa_datos/configuracion/clases/metodos_datos.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data.SqlClient
+Public Class metodos_datos
+    Public Shared Function CrearComando() As SqlCommand
+        Dim cadenaConexion = configuracion.CadenaConexion
+        Dim conexion As New SqlConnection()
+        conexion.ConnectionString = cadenaConexion
+        Dim comando As New SqlCommand()
+        comando = conexion.CreateCommand()
+        comando.CommandType = CommandType.Text
+        Return comando
+    End Function
+
+    Public Shared Function CrearComando_sp() As SqlCommand                          'Comando para los Stored Procedures
+        Dim cadenaConexion = configuracion.CadenaConexion
+        Dim conexion As New SqlConnection()
+        conexion.ConnectionString = cadenaConexion
+        Dim comando As New SqlCommand()
+        comando = conexion.CreateCommand()
+        comando.CommandType = CommandType.StoredProcedure
+        Return comando
+    End Function
+
+    Public Shared Function EjecutarComando(comando As SqlCommand) As Integer
+        Dim i As Integer
+        Try
+            comando.Connection.Open()
+            i = comando.ExecuteNonQuery()
+            Return i
+        Catch ex As Exception
+            Return -1
+        Finally
+            comando.Connection.Close()
+        End Try
+    End Function
+    Public Shared Function EjecutarBusqueda(comando As SqlCommand) As DataTable
+        Dim tabla As New DataTable
+        Dim adaptador As New SqlDataAdapter
+        Try
+            comando.Connection.Open()
+            adaptador.SelectCommand = comando
+            adaptador.Fill(tabla)
+        Catch ex As Exception
+        Finally
+            comando.Connection.Close()
+        End Try
+        Return tabla
+    End Function
+End Class
