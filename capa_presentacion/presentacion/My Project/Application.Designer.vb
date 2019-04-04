5073917910cb4c3fa5ad614d503fcedf27383660
Jeremy <jeremyzelaya@hotmail.es>
2019-04-03 23:08:42 -0600

    Basic setup for Capa de Presentacion

--- capa_presentacion/presentacion/My Project/Application.Designer.vb
+++ capa_presentacion/presentacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.presentacion.Form1
+            Me.MainForm = Global.presentacion.dashboard
         End Sub
     End Class
 End Namespace
